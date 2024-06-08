0ccab06a85b841a0fe874053c6a2c21bea14ee5d
Venomnay <164908699+Venomnay@users.noreply.github.com>
2024-06-08 09:17:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/VeGaZoRo/VeGa",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Venom")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
